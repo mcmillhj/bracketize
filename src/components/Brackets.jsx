7dafea17e4d96a6ccc1e68f6c880268537d00efc
Hunter McMillen <mcmillhj@gmail.com>
2018-02-20 18:29:19 -0500

    use grid on brackets screen

--- src/components/Brackets.jsx
+++ src/components/Brackets.jsx
@@ -3,7 +3,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { Button, Card, Container, Header, Image, List } from 'semantic-ui-react';
+import { Button, Card, Container, Grid, Header, Image, List } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 import { deleteBracket, getBrackets } from 'state/brackets';
@@ -59,34 +59,38 @@ class Brackets extends React.Component<{
     return (
       <Container>
         <Header as="h1">My Brackets</Header>
-        {brackets &&
-          brackets.map(b => (
-            <BracketCard key={b.id}>
-              <Card.Content>
-                <BracketCardImage size="tiny" src={b.seeds[0].image} />
-                <Card.Header as="h4">{b.name}</Card.Header>
-                <Card.Meta>{new Date(b.created).toUTCString()}</Card.Meta>
-                <Card.Description>
-                  <List>
-                    <List.Item>{`Number of seeds: ${b.size}`}</List.Item>
-                    <List.Item>{`Round: ${b.round}`}</List.Item>
-                    <List.Item>{`Complete: ${b.complete}`}</List.Item>
-                  </List>
-                </Card.Description>
-              </Card.Content>
-              <Card.Content extra>
-                <BracketButton onClick={() => this.cloneBracket(b.id)} disabled>
-                  Clone Bracket
-                </BracketButton>
-                <BracketButton onClick={() => this.deleteBracket(b.id)} disabled>
-                  Delete Bracket
-                </BracketButton>
-                <BracketButton as={Link} key={b.id} to={`/brackets/${b.id}`}>
-                  View Bracket
-                </BracketButton>
-              </Card.Content>
-            </BracketCard>
-          ))}
+        <Grid columns={2} doubling stretched>
+          {brackets &&
+            brackets.map(b => (
+              <Grid.Column key={b.id}>
+                <BracketCard key={b.id}>
+                  <Card.Content>
+                    <BracketCardImage size="tiny" src={b.seeds[0].image} />
+                    <Card.Header as="h4">{b.name}</Card.Header>
+                    <Card.Meta>{new Date(b.created).toUTCString()}</Card.Meta>
+                    <Card.Description>
+                      <List>
+                        <List.Item>{`Number of seeds: ${b.size}`}</List.Item>
+                        <List.Item>{`Round: ${b.round}`}</List.Item>
+                        <List.Item>{`Complete: ${b.complete}`}</List.Item>
+                      </List>
+                    </Card.Description>
+                  </Card.Content>
+                  <Card.Content extra>
+                    <BracketButton onClick={() => this.cloneBracket(b.id)} disabled>
+                      Clone Bracket
+                    </BracketButton>
+                    <BracketButton onClick={() => this.deleteBracket(b.id)} disabled>
+                      Delete Bracket
+                    </BracketButton>
+                    <BracketButton as={Link} key={b.id} to={`/brackets/${b.id}`}>
+                      View Bracket
+                    </BracketButton>
+                  </Card.Content>
+                </BracketCard>
+              </Grid.Column>
+            ))}
+        </Grid>
       </Container>
     );
   }
