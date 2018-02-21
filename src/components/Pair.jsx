c16131789926e08bdfeac2fa1efd161c36191c53
Hunter McMillen <mcmillhj@gmail.com>
2018-02-21 16:07:41 -0500

    remove comments

--- src/components/Pair.jsx
+++ src/components/Pair.jsx
@@ -4,32 +4,25 @@ import { List } from 'semantic-ui-react';
 
 import Seed from 'components/Seed';
 
-const Pair = ({ round, currentRound, complete, elements: [A, B] }) => {
-  // console.log('ROUND = ', round);
-  // console.log('currentRound = ', currentRound);
-  // console.log('isFinalRound = ', isFinalRound);
+const Pair = ({ round, currentRound, complete, elements: [A, B] }) => (
+  <List>
+    <List.Item key={'seed-1'}>
+      <Seed
+        round={round}
+        e={A}
+        winner={(round >= currentRound && !complete) || A.votes[round - 1] >= B.votes[round - 1]}
+      />
+    </List.Item>
+    <List.Item key={'seed-2'}>
+      <Seed
+        round={round}
+        e={B}
+        winner={(round >= currentRound && !complete) || A.votes[round - 1] < B.votes[round - 1]}
+      />
+    </List.Item>
+  </List>
+);
 
-  // console.log('A = ', A);
-  // console.log('B = ', B);
-  return (
-    <List>
-      <List.Item key={'seed-1'}>
-        <Seed
-          round={round}
-          e={A}
-          winner={(round >= currentRound && !complete) || A.votes[round - 1] >= B.votes[round - 1]}
-        />
-      </List.Item>
-      <List.Item key={'seed-2'}>
-        <Seed
-          round={round}
-          e={B}
-          winner={(round >= currentRound && !complete) || A.votes[round - 1] < B.votes[round - 1]}
-        />
-      </List.Item>
-    </List>
-  );
-};
 Pair.propTypes = {
   complete: PropTypes.bool.isRequired,
   currentRound: PropTypes.number.isRequired,
