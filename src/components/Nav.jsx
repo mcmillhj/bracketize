ae768de21c84c8d31968cbd3335567a624f2b8b2
Hunter McMillen <mcmillhj@gmail.com>
2018-03-13 11:52:57 -0400

    responsive nav

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,102 +3,277 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { Link, withRouter } from 'react-router-dom';
-import { Icon, Menu, Segment } from 'semantic-ui-react';
+import { Dropdown, Icon, Menu, Responsive } from 'semantic-ui-react';
 import styled from 'styled-components';
 
 import { auth } from 'firebaze';
 
-const SquareSegment = styled(Segment)`
+const SquareMenu = styled(Menu)`
   &&& {
     border-radius: 0;
   }
 `;
 
-class Nav extends Component<{ authUser: Object | null, location: Object }, { activeItem: string }> {
+const leftItems = [
+  {
+    key: 'brackets',
+    to: '/brackets',
+    name: 'brackets',
+    requiresAuth: true,
+    active: activeItem => /brackets/.test(activeItem)
+  },
+  {
+    key: 'create-bracket',
+    to: '/create-bracket',
+    name: 'create-bracket',
+    requiresAuth: true,
+    active: activeItem => /create-bracket/.test(activeItem)
+  }
+];
+
+const rightItems = [
+  {
+    key: 'account',
+    to: '/account',
+    name: 'account',
+    requiresAuth: true,
+    active: activeItem => /account/.test(activeItem)
+  },
+  {
+    key: 'signout',
+    to: '/',
+    name: 'signout',
+    onClick: auth.doSignOut,
+    requiresAuth: true,
+    active: activeItem => /signout/.test(activeItem)
+  },
+  {
+    key: 'signin',
+    to: '/signin',
+    name: 'signin',
+    requiresAuth: false,
+    active: activeItem => /signin/.test(activeItem)
+  }
+];
+
+const NavMobile = ({ leftItems, onToggle, onItemClick, rightItems, authUser, activeItem, visible }) => (
+  <SquareMenu borderless inverted>
+    <Dropdown icon={<Icon size="large" name="bars" />} onClick={onToggle} simple={visible} item>
+      <Menu pointing stackable>
+        {leftItems
+          .filter(item => !!authUser === item.requiresAuth)
+          .map(item => (
+            <Menu.Item
+              key={item.key}
+              to={item.to}
+              as={Link}
+              name={item.name}
+              onClick={item.onClick || onItemClick}
+              active={item.active(activeItem)}
+            />
+          ))}
+      </Menu>
+    </Dropdown>
+    <Menu.Menu position="right">
+      {rightItems
+        .filter(item => !!authUser === item.requiresAuth)
+        .map(item => (
+          <Menu.Item
+            key={item.key}
+            to={item.to}
+            as={Link}
+            name={item.name}
+            onClick={item.onClick || onItemClick}
+            active={item.active(activeItem)}
+          />
+        ))}
+    </Menu.Menu>
+  </SquareMenu>
+);
+
+const NavDesktop = ({ activeItem, authUser, onItemClick, leftItems, rightItems }) => (
+  <SquareMenu borderless inverted>
+    <Menu.Item as={Link} to="/" style={{ display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
+      <Icon name="home" size="large" />
+    </Menu.Item>
+    {leftItems
+      .filter(item => !!authUser === item.requiresAuth)
+      .map(item => (
+        <Menu.Item
+          key={item.key}
+          to={item.to}
+          as={Link}
+          name={item.name}
+          onClick={item.onClick || onItemClick}
+          active={item.active(activeItem)}
+        />
+      ))}
+    <Menu.Menu position="right">
+      {rightItems
+        .filter(item => !!authUser === item.requiresAuth)
+        .map(item => (
+          <Menu.Item
+            key={item.key}
+            to={item.to}
+            as={Link}
+            name={item.name}
+            onClick={item.onClick || onItemClick}
+            active={item.active(activeItem)}
+          />
+        ))}
+    </Menu.Menu>
+  </SquareMenu>
+);
+
+class NavBar extends Component<
+  { children: any, authUser: Object, leftItems: Array<Object>, rightItems: Array<Object>, location: Object },
+  { activeItem: string, visible: boolean }
+> {
   state = {
+    visible: false,
     activeItem: ''
   };
 
   componentDidMount() {
     const { location: { pathname } } = this.props;
+
     pathname && this.handleItemClick(null, { name: pathname.replace('/', '') });
   }
 
-  handleItemClick = (_, { name }: { name: string }) => this.setState({ activeItem: name });
+  componentDidUpdate() {
+    const { location: { pathname } } = this.props;
+    const { activeItem } = this.state;
+
+    pathname &&
+      activeItem !== pathname.replace('/', '') &&
+      this.handleItemClick(null, { name: pathname.replace('/', '') });
+  }
+
+  handleItemClick = (_, { name }: { name: string }) => {
+    const { activeItem } = this.state;
+
+    activeItem !== name && this.setState({ activeItem: name });
+  };
+
+  hangleToggle = () => this.setState({ visible: !this.state.visible });
 
   render() {
-    const { activeItem } = this.state;
     const { authUser } = this.props;
+    const { activeItem, visible } = this.state;
 
     return (
-      <SquareSegment inverted>
-        <Menu size="large" inverted secondary pointing>
-          <Menu.Item as={Link} to="/">
-            <Icon name="code" size="large" />
-          </Menu.Item>
-
-          {authUser && [
-            <Menu.Item
-              key="brackets"
-              as={Link}
-              to="/brackets"
-              name="brackets"
-              active={/brackets/.test(activeItem)}
-              onClick={this.handleItemClick}>
-              Brackets
-            </Menu.Item>,
-            <Menu.Item
-              key="create-bracket"
-              as={Link}
-              to="/create-bracket"
-              name="create-bracket"
-              active={/create-bracket/.test(activeItem)}
-              onClick={this.handleItemClick}>
-              New Bracket
-            </Menu.Item>
-          ]}
-
-          <Menu.Menu position="right">
-            {authUser ? (
-              [
-                <Menu.Item
-                  key="account"
-                  as={Link}
-                  to="/account"
-                  name="account"
-                  active={/account/.test(activeItem)}
-                  onClick={this.handleItemClick}>
-                  Account
-                </Menu.Item>,
-                <Menu.Item
-                  key="signout"
-                  as={Link}
-                  to="/"
-                  name="signout"
-                  active={/signout/.test(activeItem)}
-                  onClick={auth.doSignOut}>
-                  Sign Out
-                </Menu.Item>
-              ]
-            ) : (
-              <Menu.Item
-                as={Link}
-                to="/signin"
-                name="signin"
-                active={/signin/.test(activeItem)}
-                onClick={this.handleItemClick}>
-                Login
-              </Menu.Item>
-            )}
-          </Menu.Menu>
-        </Menu>
-      </SquareSegment>
+      <div>
+        <Responsive {...Responsive.onlyMobile}>
+          <NavMobile
+            leftItems={leftItems}
+            rightItems={rightItems}
+            authUser={authUser}
+            onItemClick={this.handleItemClick}
+            activeItem={activeItem}
+            onToggle={this.hangleToggle}
+            visible={visible}
+          />
+        </Responsive>
+        <Responsive minWidth={Responsive.onlyTablet.minWidth}>
+          <NavDesktop
+            leftItems={leftItems}
+            rightItems={rightItems}
+            authUser={authUser}
+            onItemClick={this.handleItemClick}
+            activeItem={activeItem}
+          />
+        </Responsive>
+      </div>
     );
   }
 }
 
+// class Nav extends Component<{ authUser: Object | null, location: Object }, { activeItem: string }> {
+//   state = {
+//     activeItem: ''
+//   };
+
+//   componentDidMount() {
+//     const { location: { pathname } } = this.props;
+
+//     pathname && this.handleItemClick(null, { name: pathname.replace('/', '') });
+//   }
+
+//   handleItemClick = (_, { name }: { name: string }) => this.setState({ activeItem: name });
+
+//   render() {
+//     const { activeItem } = this.state;
+//     const { authUser } = this.props;
+
+//     return (
+//       <SquareSegment inverted>
+//         <Menu size="large" inverted secondary pointing>
+//           <Menu.Item as={Link} to="/">
+//             <Icon name="home" size="large" />
+//           </Menu.Item>
+
+//           {authUser && [
+//             <Menu.Item
+//               key="brackets"
+//               as={Link}
+//               to="/brackets"
+//               name="brackets"
+//               active={/brackets/.test(activeItem)}
+//               onClick={this.handleItemClick}>
+//               Brackets
+//             </Menu.Item>,
+//             <Menu.Item
+//               key="create-bracket"
+//               as={Link}
+//               to="/create-bracket"
+//               name="create-bracket"
+//               active={/create-bracket/.test(activeItem)}
+//               onClick={this.handleItemClick}>
+//               New Bracket
+//             </Menu.Item>
+//           ]}
+
+//           <Menu.Menu position="right">
+//             {authUser ? (
+//               [
+//                 <Menu.Item
+//                   key="account"
+//                   as={Link}
+//                   to="/account"
+//                   name="account"
+//                   active={/account/.test(activeItem)}
+//                   onClick={this.handleItemClick}>
+//                   Account
+//                 </Menu.Item>,
+//                 <Menu.Item
+//                   key="signout"
+//                   as={Link}
+//                   to="/"
+//                   name="signout"
+//                   active={/signout/.test(activeItem)}
+//                   onClick={auth.doSignOut}>
+//                   Sign Out
+//                 </Menu.Item>
+//               ]
+//             ) : (
+//               <Menu.Item
+//                 as={Link}
+//                 to="/signin"
+//                 name="signin"
+//                 active={/signin/.test(activeItem)}
+//                 onClick={this.handleItemClick}>
+//                 Login
+//               </Menu.Item>
+//             )}
+//           </Menu.Menu>
+//         </Menu>
+//       </SquareSegment>
+//     );
+//   }
+// }
+
 export default withRouter(
   connect(state => ({
     authUser: state.auth.authUser
-  }))(Nav)
+  }))(NavBar)
 );
